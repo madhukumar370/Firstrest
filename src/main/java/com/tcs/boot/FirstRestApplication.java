0295bd35cf84ab50c8422e49240ff38da1401142
madhukumar370 <garlapati.madhukumar370@gmail.com>
2025-12-10 09:14:01 +0000

    small change and test

--- src/main/java/com/tcs/boot/FirstRestApplication.java
+++ src/main/java/com/tcs/boot/FirstRestApplication.java
@@ -9,7 +9,7 @@ public class FirstRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstRestApplication.class, args);
 		System.out.println("JENKIS try out 1");
-		System.out.println("Try to cnnect through other projects");
+		System.out.println("Try to connect through other projects");
 	}
 
 }
